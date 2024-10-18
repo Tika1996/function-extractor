0329f05e2685aa3a7c2b1077e753790507b16fc8
Tika1996 <102831643+Tika1996@users.noreply.github.com>
2024-10-18 14:29:02 +0100

    Create index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+     import React from 'react';
+     import ReactDOM from 'react-dom';
+     import FunctionExtractor from './function-extractor';
+
+     ReactDOM.render(
+       <React.StrictMode>
+         <FunctionExtractor />
+       </React.StrictMode>,
+       document.getElementById('root')
+     );
